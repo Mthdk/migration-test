5c9ac09ca624c16a1f9c9d9aeb6440d8d328f81c
Mthdk <matheus.karo@aluno.faculdadeimpacta.com.br>
2023-08-31 13:43:43 -0300

    Update convert_tf_pdf.py

--- convert_tf_pdf.py
+++ convert_tf_pdf.py
@@ -1,33 +1,34 @@
 import os
 from PIL import Image
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
+from fpdf import FPDF
 
-def tif_to_pdf(input_path, output_path):
-    image = Image.open(input_path)
-    pdf_path = os.path.join(output_path, os.path.basename(input_path)[:-4] + '.pdf')
+def convert_tif_to_pdf(input_path, output_path):
+    # Abre a imagem TIF usando a biblioteca Pillow
+    img = Image.open(input_path)
     
-    c = canvas.Canvas(pdf_path, pagesize=letter)
-    width, height = letter
-    c.drawImage(input_path, 0, 0, width, height)
-    c.save()
+    # Cria um novo PDF usando a biblioteca FPDF
+    pdf = FPDF()
+    pdf.add_page()
+    
+    # Converte a imagem TIF para um arquivo PDF
+    pdf.image(input_path, x = 10, y = 10, w = 190)
+    
+    # Salva o PDF no local especificado
+    pdf.output(output_path, "F")
 
-def convert_images_in_directory(input_dir, output_dir):
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+def batch_convert_tif_to_pdf(input_folder, output_folder):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
     
-    for filename in os.listdir(input_dir):
+    for filename in os.listdir(input_folder):
         if filename.lower().endswith('.tif'):
-            input_path = os.path.join(input_dir, filename)
-            tif_to_pdf(input_path, output_dir)
-
-def convert_images_in_directories(input_dirs, output_dir):
-    for input_dir in input_dirs:
-        convert_images_in_directory(input_dir, output_dir)
+            input_path = os.path.join(input_folder, filename)
+            output_path = os.path.join(output_folder, filename[:-4] + '.pdf')
+            convert_tif_to_pdf(input_path, output_path)
 
 if __name__ == "__main__":
-    input_dirs = ["/caminho/do/diretorio1", "/caminho/do/diretorio2"]  # Substitua pelos caminhos dos diretórios
-    output_dir = "/caminho/do/diretorio_de_saida"  # Substitua pelo caminho do diretório de saída
+    input_folder = "/caminho/do/diretorio_com_tifs"  # Substitua pelo caminho do diretório com as imagens TIF
+    output_folder = "/caminho/do/diretorio_de_saida"  # Substitua pelo caminho do diretório de saída
 
-    convert_images_in_directories(input_dirs, output_dir)
+    batch_convert_tif_to_pdf(input_folder, output_folder)
     print("Conversão concluída. As imagens TIF foram convertidas para PDF.")
